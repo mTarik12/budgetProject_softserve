976cdf9f761b71aeadb352f3ff555bc9dffb6539
mTarik12 <tarikmayula@gmail.com>
2020-12-21 13:08:29 +0200

    new event "numpadenter"

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-const BUDGET_LOCAL_STORAGE_KEY = 'budget';
+const BUDGET_LOCAL_STORAGE_KEY = "budget";
 
-console.log('Application has started.');
+console.log("Application has started.");
 UIController.displayMonth();
 
 let budget = dbController.getBudget(BUDGET_LOCAL_STORAGE_KEY);
 
 // If local storage does not contain budget
 if (!budget) {
-    localStorage.setItem(BUDGET_LOCAL_STORAGE_KEY, JSON.stringify(budgetController.getInitialBudget()));
-    budget = budgetController.getInitialBudget();
-};
-
+  localStorage.setItem(
+    BUDGET_LOCAL_STORAGE_KEY,
+    JSON.stringify(budgetController.getInitialBudget())
+  );
+  budget = budgetController.getInitialBudget();
+}
 
 UIController.displayBudget(budget);
 UIController.displayItemsList(budget.allItems);
 
-
 const setupEventListeners = () => {
-    const DOM = UIController.getDOMstrings();
+  const DOM = UIController.getDOMstrings();
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', globalController.ctrlAddItem);
+  document
+    .querySelector(DOM.inputBtn)
+    .addEventListener("click", globalController.ctrlAddItem);
 
-    document.addEventListener('keydown', (event) => {
-        if (event.code === 'Enter') {
-            globalController.ctrlAddItem();
-        }
-    });
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Enter" || event.code === "NumpadEnter") {
+      globalController.ctrlAddItem();
+    }
+  });
 
-    document.querySelector(DOM.container).addEventListener('click', globalController.ctrlDeleteItem);
+  document
+    .querySelector(DOM.container)
+    .addEventListener("click", globalController.ctrlDeleteItem);
 
-    document.querySelector(DOM.inputType).addEventListener('change', UIController.changedType);
+  document
+    .querySelector(DOM.inputType)
+    .addEventListener("change", UIController.changedType);
 };
 setupEventListeners();
