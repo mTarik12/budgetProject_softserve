68b58f1501d8d7774a66ff8946824d70cdad0c14
mTarik12 <tarikmayula@gmail.com>
2020-12-23 12:58:13 +0200

    new alert(minus savings)

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -1,17 +1,19 @@
-// dbController is a service that implements database interface 
+// dbController is a service that implements database interface
 
 const dbController = {
-    getBudget: function () {
-        let budget = localStorage.getItem(BUDGET_LOCAL_STORAGE_KEY);
-        // console.log(budget);
-        if (budget) {
-            return JSON.parse(budget);
-        } else {
-            //throw new Error('No budget on local storage');
-            return false;
-        };
-    },
-    saveBudget: (budget) => {
-        localStorage.setItem(BUDGET_LOCAL_STORAGE_KEY, JSON.stringify(budget));
-    },
-}
+  getBudget: function () {
+    let budget = localStorage.getItem(BUDGET_LOCAL_STORAGE_KEY);
+    // console.log(budget);
+    if (budget) {
+      return JSON.parse(budget);
+    } else {
+      //throw new Error('No budget on local storage');
+      return false;
+    }
+  },
+  saveBudget: (budget) => {
+    budget.grossTotal < 0 && alert("Watch out!!! You have minus savings!");
+
+    localStorage.setItem(BUDGET_LOCAL_STORAGE_KEY, JSON.stringify(budget));
+  },
+};
